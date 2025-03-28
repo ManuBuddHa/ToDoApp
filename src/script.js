00ecd2caadcf0d47297b002eb98d2bb8ebde7784
Manu BuddHa <ferano915@gmail.com>
2025-03-28 21:48:59 +0530

    Task Add Basic Structure Created

--- src/script.js
+++ src/script.js
@@ -2,6 +2,17 @@ import './styles.css';
 import { addProject,closeDialog,projectNew } from './project';
 const Todo=[];
 const projectSubmitBtn = document.querySelector("#add");
+const taskAddBtn = document.querySelector("#task-add");
+const taskDialog = document.querySelector("#task-dialog");
+taskAddBtn.addEventListener("click",addTask);
 projectSubmitBtn.addEventListener('click',addProject);
 const close = document.querySelector("#close");
+const taskClose =document.querySelector("#task-close");
+taskClose.addEventListener("click",taskCloseDialog);
 close.addEventListener("click",closeDialog);
+function addTask(){
+   taskDialog.showModal();
+}
+function taskCloseDialog(){
+   taskDialog.close();
+}
